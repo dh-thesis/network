e1f678f6a1f825e4327271c2cd4ed1a67c6efd3b
herreio <mail@herre.io>
2019-05-20 18:54:58 +0200

    ous graph added

---
+++ assets/js/ous.js
@@ -0,0 +1,11 @@
+sigma.parsers.json('../../data/ous.json', {
+    container: 'container',
+    settings: {
+        scalingMode: "outside",
+        drawLabels: true,
+        defaultLabelSize: 10,
+        labelThreshold: 5,
+        maxNodeSize: 2
+    }
+});
+ 
