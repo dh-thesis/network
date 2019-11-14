f3bbea0aba5433ab1645dd0e2c816a8c4b14aa08
herreio <mail@herre.io>
2019-11-14 15:40:56 +0100

    keep docs only

--- scripts/utils.r
+++
@@ -1,9 +0,0 @@
-read.data <- function(x) {
-    data <- read.csv(x,
-                     sep = ",",
-                     encoding = "UTF-8",
-                     header = T,
-                     quote = '"',
-                     stringsAsFactors = F)
-    return(data)
-}
